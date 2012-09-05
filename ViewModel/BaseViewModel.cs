ba3f90c1811469decd6d82755ddfcbd512e71152
Jin. K <operator1732@gmail.com>
2012-09-06 01:29:17 +0900

    adf

---
+++ ViewModel/BaseViewModel.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace CLWD.ViewModel
+{
+    public abstract class BaseViewModel : ObservableObject, IDisposable
+    {
+        #region Fields
+
+        RelayCommand _closeCommand;
+        RelayCommand _openCommand;
+        #endregion // Fields
+
+
+        #region Constructor
+
+        protected BaseViewModel()
+        {
+        }
+
+        #endregion // Constructor
+
+        #region DisplayName
+
+        /// <summary>
+        /// Returns the user-friendly name of this object.
+        /// Child classes can set this property to a new value,
+        /// or override it to determine the value on-demand.
+        /// </summary>
+        public virtual string DisplayName { get; protected set; }
+
+        #endregion // DisplayName
+
+        #region CloseCommand
+
+        /// <summary>
+        /// Returns the command that, when invoked, attempts
+        /// to remove this workspace from the user interface.
+        /// </summary>
+        public ICommand CloseCommand
+        {
+            get
+            {
+                if (_closeCommand == null)
+                    _closeCommand = new RelayCommand( () => this.OnRequestClose());
+
+                return _closeCommand;
+            }
+        }
+
+        public ICommand OpenCommand
+        {
+            get
+            {
+                if (_openCommand == null)
+                    _openCommand = new RelayCommand(() => this.OnRequestOpen());
+
+                return _openCommand;
+            }
+        }
+        #endregion // OpenCommand
+
+
+        #region RequestClose [event]
+
+        /// <summary>
+        /// Raised when this workspace should be removed from the UI.
+        /// </summary>
+        public event EventHandler RequestClose;
+
+        public void OnRequestClose()
+        {
+            EventHandler handler = this.RequestClose;
+            if (handler != null)
+                handler(this, EventArgs.Empty);
+        }
+
+        public event EventHandler RequestOpen;
+
+        public void OnRequestOpen()
+        {
+            EventHandler handler = this.RequestOpen;
+            if (handler != null)
+                handler(this, EventArgs.Empty);
+        }
+        #endregion // RequestClose [event]
+        #region IDisposable Members
+
+        /// <summary>
+        /// Invoked when this object is being removed from the application
+        /// and will be subject to garbage collection.
+        /// </summary>
+        public void Dispose()
+        {
+            this.OnDispose();
+        }
+
+        /// <summary>
+        /// Child classes can override this method to perform 
+        /// clean-up logic, such as removing event handlers.
+        /// </summary>
+        protected virtual void OnDispose()
+        {
+        }
+
+#if DEBUG
+        /// <summary>
+        /// Useful for ensuring that ViewModel objects are properly garbage collected.
+        /// </summary>
+        ~BaseViewModel()
+        {
+            string msg = string.Format("{0} ({1}) ({2}) Finalized", this.GetType().Name, this.DisplayName, this.GetHashCode());
+            System.Diagnostics.Debug.WriteLine(msg);
+        }
+#endif
+
+        #endregion // IDisposable Members
+
+
+
+    }
+}
